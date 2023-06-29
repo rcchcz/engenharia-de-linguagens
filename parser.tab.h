ef2406da6de2952fae0a8cd3539a2971d4eac6f8
Victor Xavier <33098936+victorbxavier@users.noreply.github.com>
2023-06-29 14:49:47 -0300

    Update parser.tab.h

--- parser.tab.h
+++ parser.tab.h
@@ -1,23 +1,22 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* Bison interface for Yacc-like parsers in C
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
-   
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -28,89 +27,103 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     ID = 258,
-     INT_NUMBER = 259,
-     FLOAT_NUMBER = 260,
-     STRING_VALUE = 261,
-     INT = 262,
-     DOUBLE = 263,
-     FLOAT = 264,
-     CHAR = 265,
-     STRING = 266,
-     BOOLEAN = 267,
-     NULL_VALUE = 268,
-     VOID = 269,
-     STRUCT = 270,
-     ENUM = 271,
-     TRUE = 272,
-     FALSE = 273,
-     WHILE = 274,
-     DO = 275,
-     SWITCH = 276,
-     CASE = 277,
-     DEFAULT = 278,
-     IF = 279,
-     ELSE = 280,
-     ELSE_IF = 281,
-     FOR = 282,
-     CONTINUE = 283,
-     BREAK = 284,
-     CONST = 285,
-     STATIC = 286,
-     RETURN = 287,
-     IMPORT = 288,
-     MAIN = 289,
-     OPEN_PAREN = 290,
-     CLOSE_PAREN = 291,
-     OPEN_BRACK = 292,
-     CLOSE_BRACK = 293,
-     BLOCK_BEGIN = 294,
-     BLOCK_END = 295,
-     SEMI = 296,
-     COLON = 297,
-     DOT = 298,
-     COMMA = 299,
-     PLUS = 300,
-     MINUS = 301,
-     DIV = 302,
-     MULT = 303,
-     INCREMENT = 304,
-     DECREMENT = 305,
-     MODULE = 306,
-     ASSIGN = 307,
-     ADD_ASSIGN = 308,
-     SUB_ASSIGN = 309,
-     EQ = 310,
-     NEQ = 311,
-     LT = 312,
-     GT = 313,
-     LE = 314,
-     GE = 315,
-     AND = 316,
-     OR = 317,
-     NOT = 318
-   };
+#ifndef YY_YY_PARSER_TAB_H_INCLUDED
+# define YY_YY_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
 #endif
 
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    ID = 258,                      /* ID  */
+    INT_NUMBER = 259,              /* INT_NUMBER  */
+    FLOAT_NUMBER = 260,            /* FLOAT_NUMBER  */
+    STRING_VALUE = 261,            /* STRING_VALUE  */
+    INT = 262,                     /* INT  */
+    DOUBLE = 263,                  /* DOUBLE  */
+    FLOAT = 264,                   /* FLOAT  */
+    CHAR = 265,                    /* CHAR  */
+    STRING = 266,                  /* STRING  */
+    BOOLEAN = 267,                 /* BOOLEAN  */
+    NULL_VALUE = 268,              /* NULL_VALUE  */
+    VOID = 269,                    /* VOID  */
+    STRUCT = 270,                  /* STRUCT  */
+    ENUM = 271,                    /* ENUM  */
+    TRUE = 272,                    /* TRUE  */
+    FALSE = 273,                   /* FALSE  */
+    WHILE = 274,                   /* WHILE  */
+    DO = 275,                      /* DO  */
+    SWITCH = 276,                  /* SWITCH  */
+    CASE = 277,                    /* CASE  */
+    DEFAULT = 278,                 /* DEFAULT  */
+    IF = 279,                      /* IF  */
+    ELSE = 280,                    /* ELSE  */
+    ELSE_IF = 281,                 /* ELSE_IF  */
+    FOR = 282,                     /* FOR  */
+    CONTINUE = 283,                /* CONTINUE  */
+    BREAK = 284,                   /* BREAK  */
+    CONST = 285,                   /* CONST  */
+    STATIC = 286,                  /* STATIC  */
+    RETURN = 287,                  /* RETURN  */
+    IMPORT = 288,                  /* IMPORT  */
+    MAIN = 289,                    /* MAIN  */
+    OPEN_PAREN = 290,              /* OPEN_PAREN  */
+    CLOSE_PAREN = 291,             /* CLOSE_PAREN  */
+    OPEN_BRACK = 292,              /* OPEN_BRACK  */
+    CLOSE_BRACK = 293,             /* CLOSE_BRACK  */
+    BLOCK_BEGIN = 294,             /* BLOCK_BEGIN  */
+    BLOCK_END = 295,               /* BLOCK_END  */
+    SEMI = 296,                    /* SEMI  */
+    COLON = 297,                   /* COLON  */
+    DOT = 298,                     /* DOT  */
+    COMMA = 299,                   /* COMMA  */
+    PLUS = 300,                    /* PLUS  */
+    MINUS = 301,                   /* MINUS  */
+    DIV = 302,                     /* DIV  */
+    MULT = 303,                    /* MULT  */
+    INCREMENT = 304,               /* INCREMENT  */
+    DECREMENT = 305,               /* DECREMENT  */
+    MODULE = 306,                  /* MODULE  */
+    ASSIGN = 307,                  /* ASSIGN  */
+    ADD_ASSIGN = 308,              /* ADD_ASSIGN  */
+    SUB_ASSIGN = 309,              /* SUB_ASSIGN  */
+    EQ = 310,                      /* EQ  */
+    NEQ = 311,                     /* NEQ  */
+    LT = 312,                      /* LT  */
+    GT = 313,                      /* GT  */
+    LE = 314,                      /* LE  */
+    GE = 315,                      /* GE  */
+    AND = 316,                     /* AND  */
+    OR = 317,                      /* OR  */
+    NOT = 318                      /* NOT  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
 
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+union YYSTYPE
 {
-
-/* Line 1676 of yacc.c  */
-#line 18 "parser.y"
+#line 17 "parser.y"
 
 	int    iValue; 	/* integer value */
 	char   cValue; 	/* char value */
@@ -119,16 +132,19 @@ typedef union YYSTYPE
 	double dvalue;  /* double value */ 
 	struct record * rec;
 
+#line 136 "parser.tab.h"
 
-
-/* Line 1676 of yacc.c  */
-#line 126 "parser.tab.h"
-} YYSTYPE;
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
 
+int yyparse (void);
+
+
+#endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */

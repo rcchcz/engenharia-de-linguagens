a66d7a6e1bf2c53f540967ef2c3b0018ddc8df02
Humberto Vitalino <56692368+humberto789@users.noreply.github.com>
2023-07-04 16:40:55 -0300

    Update symbolTable.h

--- lib/symbolTable.h
+++ lib/symbolTable.h
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdio.h>
 
 #define SIZE 40
 
@@ -14,20 +15,19 @@ typedef struct word {
 typedef struct htItem{
         char * key;
         word * value;     
-        htItem * next;                                                                                                                                          
+        struct htItem * next;                                                                                                                                          
 } htItem;
 
 typedef struct hashTable{
-        htItem ** items;
+        struct htItem ** items;
 } hashTable;
 
-hashTable table;
 
 
 unsigned int hashf(char * str);
 hashTable * htCreate();
-htItem * itemPair(char *key, char * idName, char * dataType, char * symbolType, int lineNo);
-void insert (hashTable *ht, char * key, char * idName, char * dataType, char * symbolType, int lineNo);
+htItem * ht_pair(char *key, char * idName, char * dataType, char * symbolType, int lineNo);
 word * ht_get(hashTable *hashtable, char *key);
+void insert (hashTable *ht, char * key, char * idName, char * dataType, char * symbolType, int lineNo);
 void ht_del(hashTable *hashtable, char *key);
 void ht_dump(hashTable *hashtable);
